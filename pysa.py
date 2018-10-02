5f01355fbc7b1f2a72f72710ee23661aa62b8ff0
Arilton <arilton@iMac-de-LAIS-IS-7.local>
2018-10-02 15:20:06 -0300

    Insert insertion_sort function.

--- pysa.py
+++ pysa.py
@@ -86,3 +86,19 @@ def cocktail_sort(l):
                 swapped = True
 
     return l
+
+
+def insertion_sort(l):
+    
+    for i in range(1, len(l)):
+
+        key = l[i]
+
+        j = i - 1
+        while j >= 0 and key < l[j]:
+            l[j + 1] = l[j]
+            j -= 1
+
+        l[j + 1] = key
+
+    return l
